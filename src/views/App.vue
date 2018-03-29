93e1879d3f4757bd583814029047099ff1d13c25
Wizard67 <wizard67.hjn@gmail.com>
2018-03-29 15:35:42 +0800

    modify: 页面基本组织结构

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{name:'home'}">Home</router-link> |
-      <router-link :to="{name:'about'}">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
+
+<style src="@/assets/style/reset.scss" lang="scss"></style>
+<style src="./style.scss" lang="scss"></style>
