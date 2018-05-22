b209a14d8818731533976c19aa57f52bca813a84
Wizard67 <wizard67.hjn@gmail.com>
2018-05-23 00:29:38 +0800

    modify: 以 index.vue 页面为 router 匹配对象

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { importAll } from '@/utils'
 
-const routers = require.context('@/views', true, /Page\.vue$/)
+const routers = require.context('@/views', true, /index\.vue$/)
 const moduleRouters = importAll(routers, 'router')
 
 Vue.use(Router)
