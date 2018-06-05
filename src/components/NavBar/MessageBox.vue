0e4031d720977a66bd9261163a150e5ef9485816
Wizard67 <wizard67.hjn@gmail.com>
2018-06-05 23:28:18 +0800

    add: MeaasgeBox 组件

---
+++ src/components/NavBar/MessageBox.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-popover style="margin-left: 25px;"
+    placement="bottom-end"
+    width="280"
+    trigger="click">
+
+    <el-badge class="fix-badge"
+      is-dot
+      slot="reference"
+    >
+      <i class="el-icon-icon-bell"></i>
+    </el-badge>
+
+    <el-tabs class="fix-tabs">
+
+      <el-tab-pane :label="`通知${getMessageLength(messages.notify)}`" name="notify">
+        <MessageBoxItem :content="messages.notify" />
+      </el-tab-pane>
+      <el-tab-pane :label="`消息${getMessageLength(messages.messages)}`" name="messages">
+        <MessageBoxItem :content="messages.messages" />
+      </el-tab-pane>
+      <el-tab-pane :label="`待办${getMessageLength(messages.schedule)}`" name="schedule">
+        <MessageBoxItem :content="messages.schedule" />
+      </el-tab-pane>
+
+    </el-tabs>
+  </el-popover>
+</template>
+
+<script>
+import MessageBoxItem from '@/components/NavBar/MessageBoxItem'
+
+export default {
+  props: {
+    messages: {
+      type: Object,
+    }
+  },
+  methods: {
+    getMessageLength(arr) {
+      return arr.length > 0 ? `(${arr.length})` : ''
+    }
+  },
+  components: {
+    MessageBoxItem,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@nn-yy/scss-flex";
+
+.fix-badge {
+  display: block;
+  cursor: pointer;
+  i {
+    display: block;
+  }
+}
+
+.fix-tabs {
+  /deep/ {
+    .el-tabs__nav-wrap {
+      @include flex( center, middle );
+    }
+  }
+}
+</style>
+
+
---
+++ src/components/NavBar/MessageBoxItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="content.length">
+    <template v-for="(item, index) in content">
+      <p :key="index">
+        {{item.main}}
+      </p>
+    </template>
+  </div>
+  <div v-else>
+      暂无数据
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Array,
+    }
+  }
+}
+</script>
