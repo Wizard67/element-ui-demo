4fa95f74ae38376f3f5ff21b332ddc17f2ed116c
Wizard67 <wizard67.hjn@gmail.com>
2018-06-15 13:11:29 +0800

    modify: 使用 mock api 数据

--- src/App.vue
+++ src/App.vue
@@ -48,144 +48,22 @@ import SearchFiled from '@/components/NavBar/SearchFlied'
 import UserDropdown from '@/components/NavBar/UserDropdown'
 import MessageBox from '@/components/NavBar/MessageBox'
 
+import { mapState } from 'vuex'
+
 export default {
   name: 'App',
   data() {
     return {
-      isCollapse: false,
-      userInfo: {
-        userName: 'Wizrad67',
-        avatar: '/avatar.jpg',
-      },
-      nav: [
-        {
-          title: 'dashboard',
-          icon: 'icon-dashboard',
-          childs: [
-            {
-              title: '分析页',
-              index: '1-1',
-            },
-            {
-              title: '监控页',
-              index: '1-2',
-            },
-            {
-              title: '工作台',
-              index: '1-3',
-            },
-          ]
-        },
-        {
-          title: '表单页',
-          icon: 'icon-edit-square-o',
-          childs: [
-            {
-              title: '基础表单',
-              index: '2-1',
-            },
-            {
-              title: '分步表单',
-              index: '2-2',
-            },
-            {
-              title: '高级表单',
-              index: '2-3',
-            },
-          ]
-        },
-        {
-          title: '列表页',
-          icon: 'icon-table',
-          childs: [
-            {
-              title: '查询表格',
-              index: '3-1',
-            },
-            {
-              title: '标准列表',
-              index: '3-2',
-            },
-            {
-              title: '卡片列表',
-              index: '3-3',
-            },
-            {
-              title: '搜索列表',
-              index: '3-4',
-            },
-          ]
-        },
-        {
-          title: '详情页',
-          icon: 'icon-unorderedlist',
-          childs: [
-            {
-              title: '基础详情页',
-              index: '4-1',
-            },
-            {
-              title: '高级详情页',
-              index: '4-2',
-            },
-          ]
-        },
-        {
-          title: '结果页',
-          icon: 'icon-check-close-circle-o',
-          childs: [
-            {
-              title: '成功',
-              index: '5-1',
-            },
-            {
-              title: '失败',
-              index: '5-2',
-            },
-          ]
-        },
-        {
-          title: '异常页',
-          icon: 'icon-error',
-          childs: [
-            {
-              title: '403',
-              index: '6-1',
-            },
-            {
-              title: '404',
-              index: '6-2',
-            },
-            {
-              title: '500',
-              index: '6-3',
-            },
-          ]
-        },
-      ],
-      suggestions: [
-        { message: '搜索提示一'},
-        { message: '搜索提示二'},
-        { message: '搜索提示三'},
-      ],
-      messages: {
-        notify: [
-            {
-              main: '通知1',
-            },
-            {
-              main: '通知2',
-            },
-        ],
-        messages: [
-            {
-              main: '消息',
-            },
-        ],
-        schedule: [
-        ],
-      },
+      isCollapse: false
     }
+  },
+  computed: {
+    ...mapState([
+      'userInfo',
+      'nav',
+      'suggestions',
+      'messages'
+    ])
   },
   methods: {
     handleCollapse(value) {
@@ -194,6 +72,9 @@ export default {
     handleSearch(value) {
       alert(value)
     }
+  },
+  created() {
+    this.$store.dispatch('initApp')
   },
   components: {
     SideBar,
--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
+import ajax from '@/ajax'
+
 const state = {
-    name: 'nihao'
+  nav: [],
+  userInfo: {
+    userName: 'Wizrad67',
+    avatar: '/avatar.jpg'
+  },
+  messages: {},
+  suggestions: [
+    { message: '搜索提示一' },
+    { message: '搜索提示二' },
+    { message: '搜索提示三' }
+  ]
 }
 
 const getters = {
@@ -7,16 +19,27 @@ const getters = {
 }
 
 const mutations = {
+  setMessages(store, value) {
+    store.messages = value
+  },
 
+  setNav(store, value) {
+    store.nav = value
+  }
 }
 
 const actions = {
-
+  initApp({commit}) {
+    ajax('initApp').then( ({payload}) => {
+      commit('setMessages', payload.messages)
+      commit('setNav', payload.nav)
+    })
+  }
 }
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
+  state,
+  getters,
+  actions,
+  mutations
 }
