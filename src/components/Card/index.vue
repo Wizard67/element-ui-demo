d878210a548e17bb4a91c773a5a67adee602ff34
Wizard67 <wizard67.hjn@gmail.com>
2018-06-20 14:58:02 +0800

    add: Card component

---
+++ src/components/Card/index.vue
@@ -0,0 +1,29 @@
+<template functional>
+  <el-card shadow="never">
+    <div class="card">
+      <template v-if="slots().head">
+      <div class="card__head">
+        <slot name="head"></slot>
+      </div>
+      </template>
+      <template v-if="slots().content">
+      <div class="card__content" :style="{ height: props.height? props.height: 'auto'}">
+        <slot name="content"></slot>
+      </div>
+      </template>
+      <template v-if="slots().foot">
+        <div class="card__foot">
+          <slot name="foot"></slot>
+        </div>
+      </template>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style src="./style.scss" lang="scss"></style>
