bbdc660f6869b75945669ed7233404ea1bf7dedb
Wizard67 <wizard67.hjn@gmail.com>
2018-06-27 19:15:39 +0800

    add: ChartCard

---
+++ src/views/Dashboard/Analysis/ChartCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <Card>
+    <template slot="head">
+      <span>{{title}}</span>
+      <el-tooltip effect="dark" :content="tip" placement="top">
+        <i class="el-icon-icon-question-circle-o"></i>
+      </el-tooltip>
+    </template>
+    <template slot="content">
+      <template v-if="$slots.content">
+        <div class="font--focus">
+          <slot name="content"></slot>
+        </div>
+      </template>
+      <Chart :options="mergeOptions" :auto-resize="true" style="width: 100%;" :style="{height: height}"></Chart>
+    </template>
+    <template slot="foot">
+      <slot name="foot"></slot>
+    </template>
+  </Card>
+</template>
+
+<script>
+import Card from "@/components/Card"
+import ECharts from "vue-echarts/components/ECharts.vue"
+
+export default {
+  name: 'ChartCard',
+  props: {
+    height: {
+      type: String,
+      default: '100px'
+    },
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    tip: {
+      type: String,
+      default: 'Tip'
+    },
+    options: {
+      type: Object,
+      required: true
+    },
+    value: {
+      type: [Array, Object],
+      default: () => []
+    }
+  },
+  computed: {
+    mergeOptions() {
+      this.options.series[0].data = this.value
+      return this.options
+    }
+  },
+  components: {
+    Card,
+    Chart: ECharts
+  }
+}
+</script>
