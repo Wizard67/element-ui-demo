3cadb56e249d9522a29de2c20e3876d6d8ba9317
Wizard67 <wizard67.hjn@gmail.com>
2018-07-03 13:21:03 +0800

    chore: 从 package.json 中分离插件配置

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+        'trailingComma': 'all'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
