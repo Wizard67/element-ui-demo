bc9e938ec91258f97c9802a79b8a51d2e173c262
Wizard67 <wizard67.hjn@gmail.com>
2018-07-16 17:04:48 +0800

    feat: 添加 getWindowSizeType 获取当前窗口宽度类型

--- src/utils/vue.js
+++ src/utils/vue.js
@@ -41,3 +41,21 @@ export const messageTips = res => {
     type: type
   });
 };
+
+export const getWindowSizeType = width => {
+  let type;
+  const breakPoint = {
+    xs: [0, 769],
+    sm: [769, 992],
+    md: [992, 1200],
+    lg: [1200, 1920],
+    xl: [1920, 4096]
+  };
+  for (const key in breakPoint) {
+    if (width >= breakPoint[key][0] && width < breakPoint[key][1]) {
+      type = key;
+      break;
+    }
+  }
+  return type;
+};
