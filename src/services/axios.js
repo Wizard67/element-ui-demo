bda8d1059571e965ddbbe56d42f16a81b8da034a
Wizard67 <wizard67.hjn@gmail.com>
2018-08-01 17:44:54 +0800

    feat(services): 取消重复请求

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,16 +1,42 @@
 import axios from 'axios'
 
-axios.defaults.timeout = 10000
+axios.defaults.baseURL = '/'
+axios.defaults.timeout = 20000
+axios.defaults.withCredentials = true
+
+// cancel the same request
+const pending = []
+const CancelToken = axios.CancelToken
+const checkPendingStatus = (config, cancel) => {
+  const flag = `${config.url}&${config.method}`
+  if (pending.indexOf(flag) !== -1) {
+    if (cancel) {
+      cancel()
+    } else {
+      pending.splice(pending.indexOf(flag), 1)
+    }
+  } else {
+    if (cancel) pending.push(flag)
+  }
+}
 
 // add a request interceptor
 axios.interceptors.request.use(
-  config => config,
+  config => {
+    config.cancelToken = new CancelToken((cancel) => {
+      checkPendingStatus(config, cancel)
+    })
+    return config
+  },
   error => Promise.reject(error)
 )
 
 // add a response interceptor
 axios.interceptors.response.use(
-  response => response.data,
+  response => {
+    checkPendingStatus(response.config)
+    return response.data
+  },
   error => {
     return {
       status: 500,
