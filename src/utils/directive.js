896379bb71655de9299b551e1b2a2d7d9690c0e8
Wizard67 <wizard67.hjn@gmail.com>
2018-08-05 00:26:59 +0800

    refactor(utils): directiveCountDown 指令现在接受一个 promise 对象

--- src/utils/directive.js
+++ src/utils/directive.js
@@ -2,22 +2,23 @@ export const directiveCountDown = {
   inserted: (el, binding, vnode) => {
     vnode.event$ = () => {
       const defaultText = el.innerText
-      binding.value()
 
-      const timer = i => () => {
-        if (i < 1) {
-          el.classList.remove('is-disabled')
-          el.disabled = false
-          el.innerText = defaultText
-        } else {
-          el.classList.add('is-disabled')
-          el.disabled = true
-          el.innerText = `${i} s`
+      binding.value().then(res => {
+        const timer = i => () => {
+          if (i < 1) {
+            el.classList.remove('is-disabled')
+            el.disabled = false
+            el.innerText = defaultText
+          } else {
+            el.classList.add('is-disabled')
+            el.disabled = true
+            el.innerText = `${i} s`
+          }
         }
-      }
-      for (let i = 60; i > -1; i--) {
-        setTimeout(timer(i), 1000 * (60 - i))
-      }
+        for (let i = 60; i > -1; i--) {
+          setTimeout(timer(i), 1000 * (60 - i))
+        }
+      })
     }
     el.addEventListener('click', vnode.event$)
   },
