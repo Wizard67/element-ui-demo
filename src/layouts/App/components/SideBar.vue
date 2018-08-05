ea428a10261e60ce15c4271917c10c4b219735c4
Wizard67 <wizard67.hjn@gmail.com>
2018-08-06 00:00:58 +0800

    fix(typo): varible name

--- src/layouts/App/components/SideBar.vue
+++ src/layouts/App/components/SideBar.vue
@@ -3,13 +3,13 @@
     <LogoWrap type="dark" :logo="logo">{{ title }}</LogoWrap>
     <template v-for="(item, index) in nav">
 
-      <template v-if="item.childs && item.childs.length > 0">
+      <template v-if="item.children && item.children.length > 0">
         <el-submenu :index="item.index ?item.index :`${index}`" :key="index">
           <template :slot="'title'">
             <el-icon :name="item.icon"/>
             <span :slot="'title'">{{ item.title }}</span>
           </template>
-          <template v-for="(citem, cindex) in item.childs">
+          <template v-for="(citem, cindex) in item.children">
             <el-menu-item :index="citem.index" :key="cindex">
               <template v-if="citem.icon && citem.icon">
                 <el-icon :name="citem.icon"/>
