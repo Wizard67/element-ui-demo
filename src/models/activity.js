047c82a873a0fea07645edb0557627795130e430
Wizard67 <wizard67.hjn@gmail.com>
2018-08-09 09:02:10 +0800

    style: format code

--- src/models/activity.js
+++ src/models/activity.js
@@ -1,20 +1,9 @@
 import { request } from '@/services'
 
 export default {
-  state: {
-    messages: {},
-    suggestions: [
-      { message: '搜索提示一' },
-      { message: '搜索提示二' },
-      { message: '搜索提示三' }
-    ]
-  },
+  state: {},
 
-  mutations: {
-    setMessages (store, value) {
-      store.messages = value
-    }
-  },
+  mutations: {},
 
   getters: {},
 
