9bb97e19e78da6ae3ff3a75b1fdd7c24e6842670
Wizard67 <wizard67.hjn@gmail.com>
2018-11-22 23:51:50 +0800

    chore: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
@@ -12,12 +12,10 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  },
-  parserOptions: {
+    parser: 'babel-eslint',
     ecmaFeatures: {
       legacyDecorators: true,
-      experimentalObjectRestSpread: true,
+      experimentalObjectRestSpread: true
     }
   }
 }
