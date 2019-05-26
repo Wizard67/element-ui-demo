58e4ca5ec6ab7572b52355e02e15a33e32a02fb3
Wizard67 <wizard67.hjn@gmail.com>
2019-05-26 23:28:10 +0800

    chore: upgrade dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, './', dir)
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   lintOnSave: false,
   chainWebpack: config => {
     config.resolve.alias
